a3dfb497765c9aa46d8570e9bd32bd6b0c991da5
Kishan_Gupta <guptakishan428@gmail.com>
2025-01-22 17:16:19 +0530

    added refresh token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,20 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const cookieParser = require("cookie-parser")
 
 const router = express.Router();
+router.use(cookieParser())
 
+
+//generate access token
+const generateAccessToken = (user) => {
+  return jwt.sign({id: user._id, role: user.role}, process.env.JWT_SECRET, {expiresIn: "15m",})
+}
+//generate refres token
+const generateRefreshToken = (user) => {
+  return jwt.sign({id: user._id}, process.env.REFRESH_SECRET, {expiresIn: "7d"})
+}
 //Register user
 router.post("/register", async (req, res) => {
   try {
@@ -37,17 +48,34 @@ router.post("/login", async function (req, res) {
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials!" });
 
-    const token = jwt.sign(
-      { userId: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "1h",
-      }
-    );
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict"
+    })
     res.json({ token });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
+router.post("/refresh", (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) return res.status(401).json({ message: "Unauthorized" })
+  
+  jwt.verify(refreshToken, process.env.REFRESH_SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ message: "Forbidden" })
+    const newAccessToken = generateAccessToken({ id: decoded.id })
+    res.json({accessToken: newAccessToken})
+  });
+})
+
+//logout route
+router.post("/logout", (req, res) => {
+  res.clearCookie("refreshToken");
+  res.json({message: "Logged out successfully"})
+})
 module.exports = router;
